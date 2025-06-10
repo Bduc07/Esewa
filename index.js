8d6f2266cc305d8b24ffbefb681593aaddf56644
Bduc07 <bidusigurung9@gmail.com>
2025-06-10 14:44:23 +0545

    Initial commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+app.use(bodyParser.json());
+
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+connectToMongo()
+app.listen(3001, () => {
+  console.log("Backend listening at http://localhost:3001");
+});
